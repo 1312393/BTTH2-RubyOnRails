f1c6970aa8486ee6d5c55661e40ef353d961755f
Nguyen <1312393@student.hcmus.edu.vn>
2016-10-05 20:02:37 +0700

    upload

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BTTH2::Application.config.secret_key_base = '1e22dda19ed8cc90d22cb98618ac50a8632a2a524ffdaa15721e8568895449a784cac4471cb623cb6285fd0b6b6a1ec7a5e467f6042a1bc3ac38b276ba5251f1'
